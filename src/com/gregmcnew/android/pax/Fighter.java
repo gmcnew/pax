b83b314f274e8ef243cd2e92071cf8ce4d6d95f4
Cam Smith <darkassasin520@gmail.com>
2011-06-16 20:38:32 -0700

    Made it so that the lasers move. Also fixed something else random that I can't think of right now.

--- src/com/gregmcnew/android/pax/Fighter.java
+++ src/com/gregmcnew/android/pax/Fighter.java
@@ -5,8 +5,8 @@ public class Fighter extends Ship {
 	public static final int HEALTH = 40;
 	public static final float DIAMETER = 7.5f;
 	public static final float TURN_SPEED = 0.025f; //in radians per 40ms
-	public static final float[]ACCELERATIONLIMS = {120f, 60f};
-	public static final float MAXSPEED = 150f;
+	public static final float[]ACCELERATIONLIMS = {120, 60};
+	public static final float MAXSPEED = 150;
 	public static final int[] TARGET_PRIORITIES = { Entity.BOMBER, Entity.FIGHTER, Entity.FRIGATE, Entity.FACTORY };
 
 	protected Fighter() {
--- src/com/gregmcnew/android/pax/Laser.java
+++ src/com/gregmcnew/android/pax/Laser.java
@@ -4,20 +4,27 @@ public class Laser extends Projectile {
 
 	public static final int HEALTH = 40;
 	public static final float DIAMETER = 3;
-	public static final float INITIAL_VELOCITY = 3;
+	public static final float INITIAL_VELOCITY = 350;
 	public static final float TURN_SPEED = 0;
-	public static final float[] ACCELERATIONLIMS = {0f, 0f};
-	public static final float MAXVELOCITY = 3f;
+	public static final float[] ACCELERATIONLIMS = {0, 0};
+	public static final float MAXVELOCITY = 3;
 	public static final int MAX_LIFE_MS = 1000;
 
 	protected Laser(Ship parent) {
 		super(Entity.LASER, null, null, MAX_LIFE_MS, HEALTH, DIAMETER, TURN_SPEED, ACCELERATIONLIMS, MAXVELOCITY);
 		body.center.set(parent.body.center);
-		velocity.set(parent.velocity);
 		heading = parent.heading;
+		targetHeading = heading;
 		
-		float headingX = (float) Math.cos(heading);
-		float headingY = (float) Math.sin(heading);
-		velocity.offset(headingX * INITIAL_VELOCITY, headingY * INITIAL_VELOCITY);
+		velocity.x = (float) Math.cos(heading) * INITIAL_VELOCITY;
+		velocity.y = (float) Math.sin(heading) * INITIAL_VELOCITY;
+	}
+	
+	@Override
+	public void updateHeading(){
+	}
+	
+	@Override
+	public void updateVelocity(){
 	}
 }
