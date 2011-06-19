72575ff25897f2cd52728f9dc54bc543afb20a43
Greg McNew <gmcnew@gmail.com>
2011-06-19 13:34:40 -0500

    Increase fighters' turn speeds to make them more useful.

--- src/com/gregmcnew/android/pax/Fighter.java
+++ src/com/gregmcnew/android/pax/Fighter.java
@@ -4,7 +4,7 @@ public class Fighter extends Ship {
 	
 	public static final int HEALTH = 40;
 	public static final float DIAMETER = 7.5f;
-	public static final float TURN_SPEED = 0.025f; //in radians per 40ms
+	public static final float TURN_SPEED = 0.075f; //in radians per 40ms
 	public static final float[]ACCELERATIONLIMS = {120, 60};
 	public static final float MAXSPEED = 150;
 	public static final int[] TARGET_PRIORITIES = { Entity.BOMBER, Entity.FIGHTER, Entity.FRIGATE, Entity.FACTORY };
