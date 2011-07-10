30f3fd61c84b60a36db5808d858efb65d458454e
Greg McNew <gmcnew@gmail.com>
2011-07-10 16:20:22 -0500

    Use the manifest's version name on the "About" screen.

--- src/com/gregmcnew/android/pax/AboutActivity.java
+++ src/com/gregmcnew/android/pax/AboutActivity.java
@@ -1,12 +1,24 @@
 package com.gregmcnew.android.pax;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Update the version string.
+        TextView versionInfoField = (TextView) findViewById(R.id.version);
+		try {
+			PackageInfo packageInfo = getPackageManager().getPackageInfo(this.getPackageName(), 0);
+		    versionInfoField.setText(String.format("version %s", packageInfo.versionName));
+		} catch (NameNotFoundException e) {
+			// Leave the version info string unchanged.
+		}
     }
 }
