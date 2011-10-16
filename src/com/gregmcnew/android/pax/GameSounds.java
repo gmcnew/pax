d74b3cc4f6187c0e2a90134fa89e7c64a229aee5
Greg McNew <gmcnew@gmail.com>
2011-10-16 02:13:09 -0500

    Lazy-initialize sounds.

--- src/com/gregmcnew/android/pax/GameSounds.java
+++ src/com/gregmcnew/android/pax/GameSounds.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.util.Log;
 
 public class GameSounds {
 	
@@ -14,23 +15,35 @@ public class GameSounds {
 	private static SoundPool sSoundPool = null;
 	
 	private static Map<Sound, Integer> sGameSounds;
+	private static Context sContext;
 	
 	public static void initialize(Context context) {
 		if (sSoundPool != null) {
 			sSoundPool.release();
+			sSoundPool = null;
 		}
+		sContext = context;
+	}
+	
+	private static void lazyInitialize() {
+		Log.v(Pax.TAG, "loading sounds");
+		
 		sGameSounds = new HashMap<Sound, Integer>();
 		sSoundPool = new SoundPool(8, AudioManager.STREAM_MUSIC, 0);
 		
-		sGameSounds.put(Sound.SHOOT_BOMB,    sSoundPool.load(context, R.raw.shoot_bomb, 0));
-		sGameSounds.put(Sound.SHOOT_LASER,   sSoundPool.load(context, R.raw.shoot_laser, 0));
-		sGameSounds.put(Sound.SHOOT_MISSILE, sSoundPool.load(context, R.raw.shoot_missile, 0));
-		sGameSounds.put(Sound.EXPLOSION,     sSoundPool.load(context, R.raw.explode_factory, 0));
-		sGameSounds.put(Sound.UPGRADE,       sSoundPool.load(context, R.raw.upgrade, 0));
+		sGameSounds.put(Sound.SHOOT_BOMB,    sSoundPool.load(sContext, R.raw.shoot_bomb, 0));
+		sGameSounds.put(Sound.SHOOT_LASER,   sSoundPool.load(sContext, R.raw.shoot_laser, 0));
+		sGameSounds.put(Sound.SHOOT_MISSILE, sSoundPool.load(sContext, R.raw.shoot_missile, 0));
+		sGameSounds.put(Sound.EXPLOSION,     sSoundPool.load(sContext, R.raw.explode_factory, 0));
+		sGameSounds.put(Sound.UPGRADE,       sSoundPool.load(sContext, R.raw.upgrade, 0));
 	}
 	
 	public static void play(Sound sound) {
-		if (sGameSounds != null && Pax.sSound) {
+		if (Pax.sSound) {
+			if (sSoundPool == null) {
+				lazyInitialize();
+			}
+			
 			// TODO: Implement directional sound.
 			Integer loadedSound = sGameSounds.get(sound);
 			if (loadedSound != null) {
